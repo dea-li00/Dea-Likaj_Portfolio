12ee66c9b466a32b456f443bb14e51636a3b830a
dea-li00 <154749164+dea-li00@users.noreply.github.com>
2024-03-22 13:26:38 +0000

    udpated protfolio image

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -18,7 +18,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/projects_image.jpg";
 
 const imageAltText = "desktop with books and laptop";
 
